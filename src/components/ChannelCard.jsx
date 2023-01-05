f13997afe44fbc448c099fe2e6f8c53cd17dccb5
harsh021102 <harsh021102@gmail.com>
2023-01-05 21:41:58 +0530

    Time Stamp: 1:21:56

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Box, CardContent, CardMedia, Typography} from '@mui/material'
+import {CheckCircle} from '@mui/icons-material'
+import {Link} from 'react-router-dom'
+const ChannelCard = () => {
+  return (
+    <div>ChannelCard</div>
+  )
+}
+
+export default ChannelCard
